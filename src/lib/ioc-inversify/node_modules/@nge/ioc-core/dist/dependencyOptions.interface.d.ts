34affd6b67187017b403661039958cc30c5485d6
Steve Hirscher <shirscher@gmail.com>
2017-09-11 17:34:48 -0500

    Remove node_modules

--- src/lib/ioc-inversify/node_modules/@nge/ioc-core/dist/dependencyOptions.interface.d.ts
+++
@@ -1,9 +0,0 @@
-import { IContainer } from './container.interface';
-import { LifetimeScope } from './lifetimeScope.enum';
-export interface IDependencyOptions {
-    name: string;
-    params: {
-        [key: string]: (resolver: IContainer) => any;
-    };
-    lifetime: LifetimeScope;
-}
